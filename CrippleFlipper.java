176743bc9426c3ebf30d31e436f77bbe1325e16a
dfj <david.jorm@gmail.com>
2013-06-11 16:18:59 +1000

    Re-write PoC, way more efficient

--- CrippleFlipper.java
+++ CrippleFlipper.java
@@ -2,52 +2,54 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Arrays;
+
+/*
+ * Cripple a JAR or other zip file by flipping a bit in the first local file header signature.
+ * This process can be reversed by flipping the bit back.
+ * Useful for rendering JARs non-executable.
+ */
 
 public class CrippleFlipper
 {
+	// The header value is little-endian 0x04034b50. The file can be crippled by setting it to 0x05034b50.
+	private void modifyFirstLocalFileHeaderSig(String fileName, byte val)
+	{
+                try
+                {
+                        RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+                        raf.seek(3);
+                        raf.write(new byte[]{val});
+                        raf.close();
+                }
+                catch (Exception ex)
+                {
+                        ex.printStackTrace();
+                }
+	}
+
+	public void crippleFile(String fileName)
+	{
+		byte val = 5;
+		this.modifyFirstLocalFileHeaderSig(fileName, val);
+	}
+
+        public void unCrippleFile(String fileName)
+        {
+                byte val = 4;
+                this.modifyFirstLocalFileHeaderSig(fileName, val);
+        }
+
 	public static void main(final String[] args) throws IOException
 	{
 		try
 		{
-			FileInputStream fis = new FileInputStream(new File(args[0]));
-			FileOutputStream fos = new FileOutputStream(args[1]);
-
-			byte[] sigBytes = new byte[]{80, 75, 5, 6};
-			byte[] crippleSigBytes = new byte[]{80, 75, 5, 7};
-			byte[] lastBytes = new byte[4];
-			byte[] bytes = new byte[1];
-			boolean flipComplete = false;
-
-			int retval = 0;
-			retval = fis.read(bytes);
-
-			while (retval != -1)
-			{
-				if (!flipComplete)
-				{
-					lastBytes[0] = lastBytes[1];
-					lastBytes[1] = lastBytes[2];
-					lastBytes[2] = lastBytes[3];
-					lastBytes[3] = bytes[0];
-
-					if (Arrays.equals(sigBytes, lastBytes))
-					{
-						System.out.println("Found End of central dir signature (0x06054b50), crippling by changing to 0x07054b50");
-						bytes[0]++;
-						flipComplete = true;
-					}
-					if (Arrays.equals(crippleSigBytes, lastBytes))
-					{
-						System.out.println("Found crippled End of central dir signature (0x07054b50), uncrippling by changing to 0x06054b50");
-						bytes[0]--;
-						flipComplete = true;
-					}
-				}
-				fos.write(bytes);
-				retval = fis.read(bytes);
-			}
-			fos.close();
+			CrippleFlipper cf = new CrippleFlipper();
+			if (args[1].equals("c"))
+				cf.crippleFile(args[0]);
+			if (args[1].equals("u"))
+				cf.unCrippleFile(args[0]);
 		}
 		catch (Exception ex)
 		{
